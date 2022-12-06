b93ee41360cd091f45254869fab7959f92cc09c6
Erik Uus <erik.uus@gmail.com>
2022-12-06 15:28:27 +0200

    Lisab koodiplokile heleda teema ja kopeerimise võimaluse

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,33 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const codeClipboard = require("eleventy-plugin-code-clipboard");
+const markdownIt = require('markdown-it');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(codeClipboard);
+  eleventyConfig.addPlugin(codeClipboard, {
+    successMessage: 'Kopeeritud',
+    failureMessage: 'Kopeerimine ebaõnnestus'
+  });
 
   // Copy static assets to the `dist` dir
   eleventyConfig.addPassthroughCopy("src/assets/images");
+
+
+  // https://www.npmjs.com/package/eleventy-plugin-code-clipboard
+  const markdownLibrary = markdownIt({
+    html: true,
+    breaks: true
+  }).use(codeClipboard.markdownItCopyButton, {
+    iconStyle: 'font-size: 18px; opacity: 0.5;',
+    iconClass: 'mdi mdi-content-copy',
+    buttonStyle: 'position: absolute; top: 5px; right: 10px; cursor: pointer; outline: none; opacity: 0.5',
+    title: 'Kopeeri',
+  });
+
+  eleventyConfig.setLibrary('md', markdownLibrary);
 
   // NOTE: Changing shortcodes needs restarting for the dev server
   eleventyConfig.addShortcode("icon", function(name, svgTag) {
